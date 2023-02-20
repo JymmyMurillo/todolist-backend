9213b0fd50c40e340f9fc487225d7291be63a200
Jymmy Murillo <murillojymmy@gmail.com>
2023-02-20 18:15:08 -0500

    Cambiando origen cors

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-const corsOptions ={
-    origin:'https://todolist-frontend-sand.vercel.app/', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+const corsOptions = {
+  origin: "http://localhost:3000/",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 const PORT = process.env.PORT || 5500;
 
