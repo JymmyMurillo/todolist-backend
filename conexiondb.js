72f50e6334c6f92fac0aaafa4e99abf46284d6e1
Jymmy Murillo <murillojymmy@gmail.com>
2023-02-20 18:35:59 -0500

    ..2

--- conexiondb.js
+++ conexiondb.js
@@ -4,25 +4,28 @@ dotenv.config();
 
 const connectDB = () => {
     // Método de conexión para la base de datos
-    mongoose.connect(process.env.DB_CONNECT, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    });
+    mongoose
+      .connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      })
+      .then(() => console.log("Connected successfully"))
+      .catch((err) => console.error(err));;
   
   
     // Las siguientes funciones permiten determinar el estado de la base de datos: conectada, desconectada o error en la conexión
-    mongoose.connection.on("connected", () => {
-      console.log("Base de datos conectada");
-    });
+    // mongoose.connection.on("connected", () => {
+    //   console.log("Base de datos conectada");
+    // });
   
-    mongoose.connection.on("disconnected", () => {
-      console.log("Base de datos desconectada");
-    });
+    // mongoose.connection.on("disconnected", () => {
+    //   console.log("Base de datos desconectada");
+    // });
   
-    mongoose.connection.on("error", () => {
-      console.log("Error al conectar la base de datos", error.message);
-    });
+    // mongoose.connection.on("error", () => {
+    //   console.log("Error al conectar la base de datos", error.message);
+    // });
   };
   
   module.exports = connectDB;
