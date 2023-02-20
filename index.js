efe54e62be2b0c18fce3a4dd3f195c56be3472d1
Jymmy Murillo <murillojymmy@gmail.com>
2023-02-20 18:21:49 -0500

    DesHabilitando corsOptions

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-const corsOptions = {
-  origin: "http://localhost:3000/",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: "http://localhost:3000/",
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
 
 const PORT = process.env.PORT || 5500;
 
@@ -16,8 +16,8 @@ const TodoItemRoute = require('./routes/todoItems')
 
 const app = express();
 app.use(express.json());
-app.use(cors(corsOptions));
-//app.use(cors());
+// app.use(cors(corsOptions));
+app.use(cors());
 connectDB();
 
 app.use('/',TodoItemRoute)
