7fff91ee23b9870498a53d679dee13d8d43f7531
Jymmy Murillo <murillojymmy@gmail.com>
2023-02-20 19:47:58 -0500

    ..8

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
-// const corsOptions = {
-//   origin: "http://localhost:3000/",
+ const corsOptions = {
+  origin: "http://localhost:3000",
 //   credentials: true, //access-control-allow-credentials:true
 //   optionSuccessStatus: 200,
-// };
+};
 
 const PORT = process.env.PORT || 5500;
 const app = express();
@@ -17,8 +17,8 @@ const TodoItemRoute = require('./routes/todoItems')
 
 
 app.use(express.json());
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+//app.use(cors());
 connectDB();
 
 app.use('/',TodoItemRoute)
