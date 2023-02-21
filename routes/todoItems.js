b8a261fa4894f4b0bb091037a4ac41f860d19690
Jymmy Murillo <murillojymmy@gmail.com>
2023-02-20 20:49:25 -0500

    ..17

--- routes/todoItems.js
+++ routes/todoItems.js
@@ -18,16 +18,23 @@ router.post('/api/item', async (req,res)=>{
 
 })
 
+
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+}; 
 // Consultar datos
 
-router.get('/api/items', async(req,res)=>{
-    try{
-        const allTodoItems = await todoItemsModel.find({});
-        res.status(200).json(allTodoItems)
-    }catch(err){
-        res.json(err);
-    }
-})
+router.get("/api/items", config, async (req, res) => {
+  try {
+    const allTodoItems = await todoItemsModel.find({});
+    res.status(200).json(allTodoItems);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 //Actualizar datos
 
